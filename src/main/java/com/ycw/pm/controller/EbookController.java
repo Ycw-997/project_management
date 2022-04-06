6afcb4a751eb4b6776edf62f624df1b6af685383
Ycw-997 <73353883+Ycw-997@users.noreply.github.com>
2022-04-07 02:33:51 +0800

    4/6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/ycw/pm/controller/EbookController.java
+++ src/main/java/com/ycw/pm/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.ycw.pm.controller;
 
 import com.ycw.pm.domain.Ebook;
+import com.ycw.pm.resp.CommonResp;
 import com.ycw.pm.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list= ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
