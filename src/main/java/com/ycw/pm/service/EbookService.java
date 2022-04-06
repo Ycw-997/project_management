13b91b36986615bcec341874ee1b795ddd8c847a
Ycw-997 <73353883+Ycw-997@users.noreply.github.com>
2022-04-07 02:10:39 +0800

    4/6 新增ebook数据库，增加ebook查询接口

--- src/main/java/com/ycw/pm/service/EbookService.java
+++ src/main/java/com/ycw/pm/service/EbookService.java
@@ -1,7 +1,7 @@
 package com.ycw.pm.service;
 
-import com.ycw.pm.domain.Demo;
-import com.ycw.pm.mapper.DemoMapper;
+import com.ycw.pm.domain.Ebook;
+import com.ycw.pm.mapper.EbookMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,10 +10,10 @@ import java.util.List;
 @Service
 public class EbookService {
     @Resource
-    private DemoMapper demoMapper;
+    private EbookMapper ebookMapper;
 
-    public List<Demo> list() {
-        return demoMapper.selectByExample(null);
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
     }
 
 }
