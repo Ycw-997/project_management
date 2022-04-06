ef56d58b605d7a3a2f7d9d34e173fbf476cc8af3
Ycw-997 <73353883+Ycw-997@users.noreply.github.com>
2022-04-07 02:01:49 +0800

    4/6 集成mybatis-generator，测试demo用例

---
+++ src/main/java/com/ycw/pm/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.ycw.pm.service;
+
+import com.ycw.pm.domain.Demo;
+import com.ycw.pm.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+
+}
